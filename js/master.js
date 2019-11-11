3eb41bb9c012c9c57954b8fd163a837c64e34db4
wisestMe <wyhzest@gmail.com>
2019-11-11 22:17:30 +0100

    add class toggle function to hamburger menu

--- js/master.js
+++ js/master.js
@@ -32,3 +32,12 @@ function autoType2() {
     setTimeout(autoType2, speed);
   }
 }
+
+// Hamburger menu function
+let headerNav = document.querySelector('.header-nav');
+function toggleNavClass() {
+  headerNav.classList.toggle('hide');
+}
+
+let hamburger = document.querySelector('.fa-hamburger');
+hamburger.addEventListener('click', toggleNavClass);
