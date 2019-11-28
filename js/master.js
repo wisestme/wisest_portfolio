dffecda02c7b67e474f7f124f7d080b4da8dd26d
wisestMe <wyhzest@gmail.com>
2019-11-28 12:06:21 +0100

    removes script error

--- js/master.js
+++ js/master.js
@@ -1,37 +1,4 @@
-let i = 0;
-let text = 'Teacher + Web Developer @ FMVR';
-let j = 0;
-let text2 = 'I also freelance';
-let speed = 50;
 
-let textSpace = document.getElementById('auto');
-
-function autoType() {
-  if (i < text.length) {
-    textSpace.innerHTML += text.charAt(i);
-    i++;
-    setTimeout(autoType, speed);
-  } else if (i === text.length) {
-    autoType2();
-  }
-}
-
-autoType();
-// document.addEventListener('DOMContentLoaded', function() {
-//     autoType();
-//     //autoDelete();
-//
-// }, false);
-
-let textSpace2 = document.getElementById('auto2');
-
-function autoType2() {
-  if (j < text2.length) {
-    textSpace2.innerHTML += text2.charAt(j);
-    j++;
-    setTimeout(autoType2, speed);
-  }
-}
 
 // Hamburger menu function
 let headerNav = document.querySelector('.header-nav');
