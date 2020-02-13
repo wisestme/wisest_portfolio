6c45965caeed839260010e952c1ad9bcb5cf86bf
wisestMe <wyhzest@gmail.com>
2020-02-13 09:44:04 +0100

    hide modal on click

--- js/home2.js
+++ js/home2.js
@@ -1,6 +1,17 @@
+// Get hold of UI elements
 let hamMenu = document.querySelector('.fa-hamburger');
 let modal = document.querySelector('.modal-wrapper');
+let closeIcon = document.querySelector('.fa-cross');
+
+
+// Show modal, hide hamburger
 hamMenu.addEventListener('click', function() {
 modal.classList.remove('toggle-hide');
 hamMenu.style.display = 'none';
+});
+
+// Show modal, hide hamburger
+closeIcon.addEventListener('click', function() {
+modal.classList.add('toggle-hide');
+hamMenu.style.display = 'block';
 });
