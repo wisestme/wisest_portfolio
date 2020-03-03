c44d0aa6bc5185b2dea4e0966e7519a073a27b7b
wisestMe <wyhzest@gmail.com>
2020-03-03 12:12:44 +0100

    put the files in the cache

--- cache.js
+++ cache.js
@@ -1,11 +1,29 @@
 const cacheName = 'v1';
 
 const cacheAssets = [
-	home
+	'index.html',
+	'home.html',
+	'home2.html',
+	'projects.html',
+	'css/home2.css',
+	'css/master.css',
+	'css/style.css',
+	'js/home2.js',
+	'js/master.js'
 ]
 // Call Install Event
 self.addEventListener('install', e => {
 	console.log('Service Worker: Installed');
+
+	e.waitUntil(
+		caches
+			.open(cacheName)
+			.then(cache => {
+				console.log('Service Worker: Caching Files');
+				cache.addAll(cacheAssets);
+			})
+			.then(() => self.skipWaiting())
+			);
 });
 
 // Call Activate Event
--- js/cache.js
+++
@@ -1,9 +0,0 @@
-// Call Install Event
-self.addEventListener('install', e => {
-	console.log('Service Worker: Installed');
-});
-
-// Call Activate Event
-self.addEventListener('activate', e => {
-	console.log('Service Worker: Activated');
-});
