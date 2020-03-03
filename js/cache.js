c8966fbfebd65a0309075ecd2e1a719b32ee54bb
wisestMe <wyhzest@gmail.com>
2020-03-03 11:14:08 +0100

    activate service worker

--- js/cache.js
+++ js/cache.js
@@ -2,3 +2,8 @@
 self.addEventListener('install', e => {
 	console.log('Service Worker: Installed');
 });
+
+// Call Activate Event
+self.addEventListener('activate', e => {
+	console.log('Service Worker: Activated');
+});
